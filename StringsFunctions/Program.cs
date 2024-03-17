3db39ff244ab8fdf1aae8250b3829a63ad4b16e9
Puki4 <nikitka2601@mail.ru>
2024-03-17 03:19:35 +0300

    Basic Strings Functions

---
+++ StringsFunctions/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace StringsFunctions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
