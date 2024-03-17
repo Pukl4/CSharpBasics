b69d993dc0725346fbfbcf12a0d1c58a87940175
Puki4 <nikitka2601@mail.ru>
2024-03-17 03:18:55 +0300

    Basic Data types and their conversion

---
+++ DataTypes/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Types
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //convert sting to bool
+            bool boolFromStr = bool.Parse("true");
+
+            //convert string to int
+            int intFromStr = int.Parse("100");
+
+            //convert int to string
+            string strVal = intFromStr.ToString();
+
+            //Getting the data type variable
+            Console.WriteLine($"Data type: {strVal.GetType()}");
+
+            //Explicit Conversion ( losing some data during conversion)
+            double dblNum = 12.345;
+            Console.WriteLine($"Integer: {(int)dblNum}");
+
+            //Implicit Conversion (smaller type  into larger with no data loss  during conversion)
+            int intNum = 10;
+            long longNum = intNum;
+
+        }
+    }
+}
