d8536d8014fa1c8f53d87575e5505b84b95d7856
Puki4 <nikitka2601@mail.ru>
2024-03-24 16:38:59 +0300

    Return value, functions

---
+++ Functions/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Functions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*A return value is a value that is returned from a method to the code that called it
+             * Methods with return values are called functions 
+             * Return value must be the same type as the method
+             */
+
+            //Example of a function
+            int Add(int a, int b)
+            {
+                return a + b;
+            }
+            //Calling method:
+            int sum=Add(5, 3);
+
+            //Another way of calling function:
+            int x = 5;
+            int y = 3;
+            int sum2=Add(x, y);
+
+            //We can combine functions in one:
+            int sum3=Add(Add(x, y), Add(5,6));
+        }
+    }
+}
