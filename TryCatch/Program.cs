11902b253a7d0fb40ab0cd3044eaf6d95f1bdf9e
Puki4 <nikitka2601@mail.ru>
2024-03-25 22:18:30 +0300

    Try, catch and finally syntax

---
+++ TryCatch/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace TryCatch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int Division(int x, int y)
+            {
+                return x / y;
+            }
+
+            try
+            {
+                //The program will throw an exception when we try to divide by 0
+                Division(10, 0);
+            }
+            catch (Exception ex) 
+            {
+                //Code that run when exception is thrown 
+                Console.WriteLine("An exception was thrown");
+                Console.WriteLine(ex.Message);
+            }
+            //We can use a finally block to run code after a try-cathc block 
+            finally 
+            {
+                Console.WriteLine("This code always runs");
+            }
+        }
+    }
+}
