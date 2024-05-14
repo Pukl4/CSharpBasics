dda700f3b98944ace840003ff952255e6b1094f1
Puki4 <nikitka2601@mail.ru>
2024-05-14 22:57:46 +0300

    Get/Set, Readonly

--- Classes_and_Objects/Animal.cs
+++ Classes_and_Objects/Animal.cs
@@ -7,34 +7,30 @@ using System.Threading.Tasks;
 namespace Classes_and_Objects
 {
         class Animal
-        {
-            public string name;
-            public string sound;
-            static int numOfAnimals = 0;
+        {   //When we set name and sound as private they can only be accessed through get and set
+            private string name;
+            private string sound;
+            public static int numOfAnimals = 0;
+            public const string SHELTER = "Nick's Home for Animals";
 
-            public Animal()
+            //readonly value cannot be changed
+            public readonly int idNum;
+
+            public Animal() : this("No name", "No sound") { }
+            
+            public Animal(string name) : this(name, "No sound") { }
+
+            public Animal(string name, string sound)
             {
-                name = "No Name";
-                sound = "No sound";
-                
-            }
+                SetName(name);
+                Sound = sound;
 
-            //If name is provided and sound is not
-            public Animal(string name = "No name")
-            {
-                // Keyword "This" refers to the object's name
-                this.name = name;
-                this.sound = "No sound";
-                numOfAnimals++;
+                NumOfAnimals = 1;
+                Random rnd = new Random(); 
+                idNum= rnd.Next(1, 2147483640);
             }
-
-            public Animal(string name = "No name", string sound = "No sound") : this(name)
-            {
-                this.name = name;
-                this.sound = sound;
-                numOfAnimals++;
-            }
-
+            
+            
             public void MakeSound()
             {
                 Console.WriteLine("{0} says {1}", name, sound);
@@ -45,5 +41,46 @@ namespace Classes_and_Objects
                 return numOfAnimals;
             }
 
+            public void SetName(string name)
+            {
+                if (!name.Any(char.IsDigit)) 
+                {
+                    this.name = name;
+                }
+                else
+                {
+                    this.name = "No name";
+                    Console.WriteLine("Name can't contain numbers");
+                }
+            }
+            public string GetName()
+            {
+                return name;
+            }
+
+            public string Sound
+            {
+                get { return sound; }
+                set 
+                {
+                    if(value.Length > 10)
+                    {
+                        sound = "No sound";
+                        Console.WriteLine("Sound is too long");
+                    }
+                    else
+                    {
+                        sound = value;   
+                    }
+                }
+            }
+
+            public string Owner { get; set; } = "No Owner";
+            
+            public static int NumOfAnimals
+            {
+                get { return numOfAnimals; }
+                set { numOfAnimals += value; }
+            }
         }  
 }
--- Classes_and_Objects/Program.cs
+++ Classes_and_Objects/Program.cs
@@ -4,52 +4,16 @@
     {
         static void Main(string[] args)
         {
-            Rectangle rect1;
-            rect1.length= 200;
-            rect1.width= 50;
-            Console.WriteLine("Area of rect1 : {0}", rect1.Area());
-            
-            Rectangle rect2 = new Rectangle(100, 40);
-            rect2 = rect1;
-            rect1.length = 33;
-            Console.WriteLine("Rect2.length : {0}", rect2.length);
-
-
-            Animal fox = new Animal()
-            {
-                name = "Red",
-                sound = "Raaw"
-            };
-
-            Console.WriteLine("# of Animals : {0}", Animal.GetNumAnimals());
-
-            Console.WriteLine("Area of Rectangle : {0}", ShapeMath.GetArea("rectangle", 5, 6));
-
-            //Creating a null 
-            int? randNum = null;
-
-            //Checking for null
-            if (randNum == null)
-            {
-                Console.WriteLine("randNum is null");
-            }
-            if (!randNum.HasValue)
-            {
-                Console.WriteLine("randNum is null");
-            }
+            Animal cat = new Animal();
+            cat.SetName("Whiskers");
+            cat.Sound = "Meow";
+            Console.WriteLine("The cat's name is {0} and says {1}", cat.GetName(), cat.Sound);
+            cat.Owner = "Derek";
+            Console.WriteLine("{0} owner is {1}", cat.GetName(), cat.Owner);
+            Console.WriteLine("{0} shelter id is {1}", cat.GetName(), cat.idNum);
+            Console.WriteLine("# of animals : {0}", Animal.NumOfAnimals);
         }
 
-        // Struct, basically, is user defined type
-        struct Rectangle
-        {
-            public double length;
-            public double width;
-
-            public Rectangle(double l = 1, double w = 1) { length = l; width = w; }
-            public double Area()
-            {
-                return length * width;
-            }
-        }
+        
     }
 }
