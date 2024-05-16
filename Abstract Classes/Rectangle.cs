645710d2bc8cfaf94a504421dd5647b11f2f3800
Puki4 <nikitka2601@mail.ru>
2024-05-16 23:52:49 +0300

    Abstract methods

---
+++ Abstract Classes/Rectangle.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstract_Classes
+{
+    class Rectangle : Shape 
+    {
+        public double Length {  get; set; }
+        public double Width { get; set; }
+
+        public Rectangle(double length, double width)
+        {
+            Length = length;
+            Width = width;
+        }
+
+        public override double Area()
+        {
+            return Length * Width;
+        }
+        public override void GetInfo()
+        {
+            //f2 is quantity of numbers after the comma
+            base.GetInfo();
+            Console.WriteLine($"It has a Length of {Length} and Width of {Width}");
+        }
+    }
+}
---
+++ Abstract Classes/Shape.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstract_Classes
+{
+    abstract class Shape
+    {
+        public string Name { get; set; }
+
+        //Virtual class is a class that serves as a base for other classes and can be overridden by derived classes
+        public virtual void GetInfo()
+        {
+            Console.WriteLine($"This is a {Name}");
+        }
+        public abstract double Area();
+
+    }
+}
