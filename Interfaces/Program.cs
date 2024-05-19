c25ca60051385f7eaef10929377fab2473d7a22c
Puki4 <nikitka2601@mail.ru>
2024-05-19 23:08:57 +0300

    Interface introduction

---
+++ Interfaces/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Interfaces
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Vehicle Ford = new Vehicle("Ford", 4, 160);
+            if (Ford is IDrivable)
+            {
+                Ford.Move();
+                Ford.Stop();
+            }
+            else
+            {
+                Console.WriteLine("The {0} can't be driven", Ford.Brand);
+            }
+        }
+    }
+}
---
+++ Interfaces/Vehicle.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaces
+{
+    class Vehicle : IDrivable
+    {
+        public string Brand { get; set; }
+        public Vehicle(string brand = "No brand", int wheels = 0, double speed = 0)
+        {
+            Brand = brand;
+            Wheels = wheels;
+            Speed = speed;
+        }
+        public double Speed { get; set; }
+        public int Wheels { get; set; }
+        public void Move()
+        {
+            Console.WriteLine($"The {Brand} moves forward at {Speed} mph");
+        }
+        public void Stop()
+        {
+            Console.WriteLine($"The {Brand} Stops");
+            Speed = 0;
+        }
+    }
+}
