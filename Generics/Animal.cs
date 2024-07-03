171d5227f3e3e798d9fbfc260f97b74a52dcd61e
Puki4 <nikitka2601@mail.ru>
2024-07-03 23:56:47 +0300

    Generics defining and usage

---
+++ Generics/Animal.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    class Animal
+    {
+        public string Name { get; set; }
+        public Animal(string name = "No name") {  Name = name; }
+        //Generics can be used with different data types
+        // <T> will be replaced with usable data type like <int> or <string>
+        public static void GetSum<T>(ref T num1, ref T num2)
+        {
+            double dbl = Convert.ToDouble(num1);
+            double dbl2 = Convert.ToDouble(num2);
+            Console.WriteLine($"{dbl}+{dbl2}={dbl+dbl2}");
+        }
+    }
+}
---
+++ Generics/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Globalization;
+using System.Text;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Generics
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Animal> animalList = new List<Animal>();
+
+            List<int> numList = new List<int>();
+            numList.Add(24);
+
+            animalList.Add(new Animal() { Name = "Doug" });
+            animalList.Add(new Animal() { Name = "Paul" });
+            animalList.Add(new Animal() { Name = "Sally" });
+
+            animalList.Insert(1, new Animal() { Name = "Steve" });
+            animalList.RemoveAt(1);
+            Console.WriteLine("Num of Animal : {0}",animalList.Count);
+            foreach (Animal a in animalList)
+            {
+                Console.WriteLine(a.Name);
+            }
+
+            int x=5, y=5;
+            Animal.GetSum<int>(ref x, ref y);
+            string strx = "5", stry = "4";
+            Animal.GetSum<string>(ref strx, ref stry);
+            
+            Rectangle<int> rec1 = new Rectangle<int>(20, 50);
+            Console.WriteLine(rec1.GetArea());
+
+            Rectangle<string> rec2 = new Rectangle<string>("20", "50");
+            Console.WriteLine(rec2.GetArea());
+        }
+
+        public struct Rectangle<T>
+        {
+            private T width;
+            private T length;
+
+            public T Width
+            {
+                get { return width; }
+                set { width = value; }
+            }
+
+            public T Length
+            {
+                get { return length; }
+                set { length = value; }
+            }
+
+            public Rectangle(T w, T l)
+            {
+                width = w;
+                length = l;
+            }
+
+            public string GetArea()
+            {
+                double dblWidth = Convert.ToDouble(Width);
+                double dblLength = Convert.ToDouble(Length);
+                return string.Format($"{Width}*{Length} = {dblWidth * dblLength}");
+            }
+        }
+    }
+}
