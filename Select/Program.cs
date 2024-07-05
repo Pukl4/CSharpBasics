78069c3a18ff75e7264fac5051ccd4f3b22be6f6
Puki4 <nikitka2601@mail.ru>
2024-07-05 19:49:17 +0300

    Select, Zip

---
+++ Select/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Select
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var oneToTen = new List<int>();
+            oneToTen.AddRange(Enumerable.Range(1, 10));
+
+            //Select composes each element of the sequence to a new form
+            var squares = oneToTen.Select(x => x * x);
+            foreach ( var square in squares) Console.WriteLine(square);
+
+            // Zip is used for applying function to elements of two sequences
+            // producing the sequence of the result
+            var listOne = new List<int>(new int[] {1, 3, 4});
+            var listTwo = new List<int>(new int[] {4, 6, 8});
+            var sumList = listOne.Zip(listTwo,(x,y) => x + y).ToList();
+            foreach(var item in sumList) Console.WriteLine(item);
+
+            
+
+            
+           
+
+
+        }
+    }
+}
