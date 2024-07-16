f47aa1fc7a1ddb600ae90a05757d9360eac9d0b8
Puki4 <nikitka2601@mail.ru>
2024-07-16 22:42:08 +0300

    Directory creation

---
+++ DirectoryFunctions/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace DirectoryFunctions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DirectoryInfo currDir = new DirectoryInfo(".");
+            DirectoryInfo NicksDir = new DirectoryInfo(@"C:\Пользователи\Pukich");
+
+            Console.WriteLine(NicksDir.FullName);
+            Console.WriteLine(NicksDir.Name);
+            Console.WriteLine(NicksDir.Parent);
+            Console.WriteLine(NicksDir.Attributes);
+            Console.WriteLine(NicksDir.CreationTime);
+
+            DirectoryInfo dataDir = new DirectoryInfo(@"C:\Пользователи\Pukich\C#Data");
+            dataDir.Create();
+        }
+    }
+}
