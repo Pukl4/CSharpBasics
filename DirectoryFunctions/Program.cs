5105a443b959702ffc7db24c38da6c8421ef686e
Puki4 <nikitka2601@mail.ru>
2024-07-19 05:08:40 +0300

    File I/O

--- DirectoryFunctions/Program.cs
+++ DirectoryFunctions/Program.cs
@@ -15,6 +15,29 @@
 
             DirectoryInfo dataDir = new DirectoryInfo(@"C:\Пользователи\Pukich\C#Data");
             dataDir.Create();
+            string[] customers =
+            {
+                "Bob Smith",
+                "Sally Smith",
+                "Robert Smith"
+            };
+            string textFilePath = @"C:\Пользователи\Pukich\C#Data\testfile1.txt";
+            File.WriteAllLines(textFilePath, customers);
+
+            foreach (string cust in File.ReadAllLines(textFilePath))
+            {
+                Console.WriteLine($"Customer : {cust}");   
+            }
+
+            DirectoryInfo myDataDir = new DirectoryInfo(@"C:\Пользователи\Pukich\C#Data");
+            FileInfo[] txtFiles = myDataDir.GetFiles("*.txt", SearchOption.AllDirectories);
+            Console.WriteLine("Matches :{0}", txtFiles.Length);
+
+            foreach (FileInfo file in txtFiles)
+            {
+                Console.WriteLine(file.Name);
+                Console.WriteLine(file.Length);
+            }
         }
     }
 }
