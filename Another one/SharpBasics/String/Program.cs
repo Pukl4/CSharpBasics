8aeb3772fa976a031875b48c99973551a5c03ab4
Nick Richard <nikitka2602@mail.ru>
2026-08-13 01:30:28 +0300

    Basic string declaring, storing input

---
+++ Another one/SharpBasics/String/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace String
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Declaring a string variable
+            string declaringTwo;
+
+            // Assigning a value to the declaringTwo variable
+            declaringTwo = "Declaring that way";
+
+            // Overwriting the variable name
+            declaringTwo = "Overwritten value";
+            string declaring = "Declaring the string this way";
+
+            Console.WriteLine(declaring);
+            Console.WriteLine(declaringTwo);
+
+            // Taking the user input and storing it in a variable
+            string userInput = Console.ReadLine();
+            Console.WriteLine("Output is: " + userInput);
+        }
+    }
+}
