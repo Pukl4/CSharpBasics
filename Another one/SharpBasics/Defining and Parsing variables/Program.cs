0100165350c901e7c55535d33a6ce73acc90aa7b
Pukl4 <nikitka2601@mail.ru>
2026-08-17 23:56:07 +0300

    Defining and Parsing variable

---
+++ Another one/SharpBasics/Defining and Parsing variables/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Defining_and_Parsing_variables
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Defining or setting up a variable 
+            int basenumber = 0;
+
+            Console.WriteLine("Enter a whole number:");
+
+            // Storing the user input into the variable
+            string userInput = Console.ReadLine();
+
+            basenumber = int.Parse(userInput);
+            Console.WriteLine($"The number you entered is: {basenumber}");
+        }
+    }
+}
