101d2c813b8b899dc084b5cd5369a824c0c00ed3
Pukl4 <nikitka2601@mail.ru>
2026-08-22 17:52:57 +0300

    Logical and Relational operators, if statement

---
+++ Another one/SharpBasics/Logic/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace Logic
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool verificationCompleted = false;
+            bool verificationStarted = true;
+
+            // If statements executes code only when the statement is true
+            if (verificationCompleted) { Console.WriteLine("OK"); }
+
+            /* Logical operators are: 
+                    AND  && — both statements are true
+                    OR   || - one of the statements is true
+                    NOT  !  - reverse the boolean condition 
+            */
+
+            // Won't execute code as one of the variables is false
+            if (verificationStarted && verificationCompleted) { Console.WriteLine("You can proceed"); }
+
+            // Code will be executed as verificationCompleted is reversed
+            if (verificationStarted && !verificationCompleted) { Console.WriteLine("Please try again"); }
+
+            // Code will be executed as one of the variables is true
+            if (verificationStarted || verificationCompleted) { Console.WriteLine("Processing"); }
+
+            // Relational operators are < <= > >=
+            // Boolean are false by default and turn into true only after initialization
+
+            int num1 = 10;
+            int num2 = 8;
+
+            bool isGreater = num1 > num2;
+
+            if (num1 > 5) { Console.WriteLine("Is greater"); }
+        }
+    }
+}
