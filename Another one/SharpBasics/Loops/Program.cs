b5eec8ec6b2d1b91f9b6bc3ee88df590952ad17b
Pukl4 <nikitka2601@mail.ru>
2026-08-25 23:02:40 +0300

    While loop

--- Another one/SharpBasics/Loops/Program.cs
+++ Another one/SharpBasics/Loops/Program.cs
@@ -16,7 +16,23 @@
             for (int i = 5; i >= 1; i--)
             {
                 Console.WriteLine($"countdown i = {i}");           
-            }           
+            }
+
+            // while loop runs as long as the specified boolean condition evaluates to true
+            // Syntax: while(condition) { code };
+            int index = 0;
+            bool whileExample = false;
+
+            while (!whileExample) 
+            {
+                Console.WriteLine("Still going");
+                index ++;
+                if (index >= 6) 
+                {
+                    whileExample = true;
+                };
+            };
+            Console.WriteLine($"Ended after {index} times");
         }
     }
 }
