d83c6434d7cb5684e3dae95274be9ff2ef1a4a4a
Pukl4 <nikitka2601@mail.ru>
2026-08-25 21:55:13 +0300

    For loop

---
+++ Another one/SharpBasics/Loops/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Loops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // For loop is used to run the code specific number of times
+            // Syntax: 
+            // for (initializer; condition; iterator) { code };
+            
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine($"i = {i}");
+            }
+
+            for (int i = 5; i >= 1; i--)
+            {
+                Console.WriteLine($"countdown i = {i}");           
+            }           
+        }
+    }
+}
