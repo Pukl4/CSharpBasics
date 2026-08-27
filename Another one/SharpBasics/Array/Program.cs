1bba0cc5c48fc5f75d757fd09b816e4b49104b00
Pukl4 <nikitka2601@mail.ru>
2026-08-28 00:02:22 +0300

    Simple array

---
+++ Another one/SharpBasics/Array/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // declaring an array 
+            int[] intArray = new int[5];
+
+            // assigning value to an array
+            intArray[0] = 1;
+            intArray[1] = 2;
+            intArray[2] = 3;
+            intArray[3] = 4;
+            intArray[4] = 5;
+
+            // indexes [0][1][2][3][4]
+            // content [1][2][3][4][5]
+
+            // declaring an array and set the array element values
+            int[] decIntArray = [1, 12, 14, 16];
+            string[] strArray = ["Monday", "Tueday", "Wednesday"];
+            Console.WriteLine(strArray[0] + $" {decIntArray[3]}");
+        }
+    }
+}
