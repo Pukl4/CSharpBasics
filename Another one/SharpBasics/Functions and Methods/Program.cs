6d1f2d25b0c15412c23e55acf4c402e3f678c023
Pukl4 <nikitka2601@mail.ru>
2026-09-01 01:27:11 +0300

    Methods basics

---
+++ Another one/SharpBasics/Functions and Methods/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Functions_and_Methods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SimpleMethod();
+            SimpleMethodSpec("Some text");
+        }
+        static void SimpleMethod()
+        {
+            Console.WriteLine("Hardcoded text");
+        }
+        static void SimpleMethodSpec(string spec)
+        {
+            Console.WriteLine($"{spec}");
+        }
+    }
+}
