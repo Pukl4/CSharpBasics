5579c2d1887ebffc92f78d8dda5ecb6d6d6dfe92
Pukl4 <nikitka2601@mail.ru>
2026-09-03 00:52:08 +0300

    Created a Weather Prediction

---
+++ Another one/SharpBasics/WeatherPredictorApp/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace WeatherPredictorApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Asking for user input
+            Console.WriteLine("Enter the number of days");
+            int days = int.Parse(Console.ReadLine().Trim());
+
+            int[] temperature = new int[days];
+            // Creating an array with weather conditions
+            string[] conditions = { "Sunny", "Rainy", "Foggy", "Snowy" };
+            string[] weatherConditions = new string[days];
+
+            Random random = new Random();
+        }
+    }
+}
