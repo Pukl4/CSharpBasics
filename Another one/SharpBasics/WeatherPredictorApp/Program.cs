56baa29c0340b4f6c73172a84f7b1d600e068047
Pukl4 <nikitka2601@mail.ru>
2026-09-04 00:29:44 +0300

    Added method for calculating average temperature, added logic for assigning random value to each element of an array

--- Another one/SharpBasics/WeatherPredictorApp/Program.cs
+++ Another one/SharpBasics/WeatherPredictorApp/Program.cs
@@ -4,16 +4,41 @@
     {
         static void Main(string[] args)
         {
+            Random random = new Random();
             // Asking for user input
             Console.WriteLine("Enter the number of days");
             int days = int.Parse(Console.ReadLine().Trim());
 
-            int[] temperature = new int[days];
+            double[] temperature = new double[days];
             // Creating an array with weather conditions
-            string[] conditions = { "Sunny", "Rainy", "Foggy", "Snowy" };
+            string[] conditions = { "sunny", "rainy", "foggy", "snowy" };
             string[] weatherConditions = new string[days];
 
-            Random random = new Random();
+            // Creating instance of Random class and 
+            // assigning random value to each day
+            
+            for (int i = 0; i < days; i++)
+            {
+                temperature[i] = random.Next(-20, 20);
+                weatherConditions[i] = conditions[random.Next(conditions.Length)];
+
+                while(temperature[i] > 0 && weatherConditions[i] == "snowy")
+                {
+                    weatherConditions[i] = conditions[random.Next(conditions.Length)];
+                }
+            }
+
+            for (int number = 0; number < days; number ++) 
+            { 
+                Console.WriteLine($"It is {temperature[number]} °C and {weatherConditions[number]}");
+            }
+            Console.WriteLine($"The average temperature is {CalculateAverage(temperature, days)} °C");
+        }
+        static double CalculateAverage(double[] temperature, int days)
+        {
+            double averageTemperature = 0;
+            foreach (double i in temperature) { averageTemperature += i; };
+            return (averageTemperature / days);
         }
     }
 }
