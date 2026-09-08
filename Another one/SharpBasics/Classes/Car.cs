42ddd4cfdf10724edb166c9c83433eb94fac56dd
Pukl4 <nikitka2601@mail.ru>
2026-09-08 22:58:27 +0300

    Creating a class

---
+++ Another one/SharpBasics/Classes/Car.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Channels;
+
+namespace Classes
+{
+    public class Car
+    {
+       //private modifier means that something can only be accessed from within
+       //the class where it is declared.
+        private string _brand = "";
+        private string _model = "";
+        private int _year = 0;
+
+        // Constructor is a code with the same name as a class
+        // and it is automatically called when an object of a class is created.
+        public Car(string brand, string model, int year)
+        {
+            _brand = brand;
+            _model = model;
+            _year = year;
+            Console.WriteLine(_brand + ", " + _model + ", " + _year);
+        }
+    }
+}
---
+++ Another one/SharpBasics/Classes/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace Classes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Car toyota = new Car("Toyota", "Corolla", 1999);
+            Car Honda = new Car("Honda", "Civic", 2005);
+        }
+    }
+}
