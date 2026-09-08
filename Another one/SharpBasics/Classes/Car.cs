fac33c4ac9db2e4719fbbb650701bcfd77f317e3
Pukl4 <nikitka2601@mail.ru>
2026-09-08 23:54:13 +0300

    Modifying get

--- Another one/SharpBasics/Classes/Car.cs
+++ Another one/SharpBasics/Classes/Car.cs
@@ -12,18 +12,32 @@ namespace Classes
         private string _brand = string.Empty;
         private string _model = string.Empty;
         private int _year = 0;
+        private bool _isluxury = true;
 
         // Constructor is a code with the same name as a class
         // and it is automatically called when an object of a class is created.
-        public Car(string brand, string model, int year)
+        public Car(string brand, string model, int year, bool isLuxury)
         {
             Brand = brand;
             Model = model;
             Year = year;
+            Isluxury = isLuxury;
             Console.WriteLine(Brand + ", " + Model + ", " + Year);
         }
 
-        public string Brand { get => _brand; set => _brand = value; }
+        public string Brand {
+            get 
+            {
+                if(Isluxury == true)
+                {
+                    return _brand + " (Luxury one)";
+                }
+                else
+                {
+                    return _brand;
+                }
+            }
+            set => _brand = value; }
         public string Model { get => _model;
             set { 
                 if (string.IsNullOrEmpty(value))
@@ -33,6 +47,17 @@ namespace Classes
                 _model = value;
             }
         }
+
+        // This property is with lambda expression, which is a shorthand way to define a property with a single expression.
         public int Year { get => _year; set => _year = value; }
+
+        /* Non-simplified version of the Year property:
+         * public int Year
+         * {
+         *      get { return _year; }
+         *      set { _year = value; }
+         * }
+         */
+        public bool Isluxury { get => _isluxury; set => _isluxury = value; }
     }
 }
--- Another one/SharpBasics/Classes/Program.cs
+++ Another one/SharpBasics/Classes/Program.cs
@@ -6,9 +6,10 @@ namespace Classes
     {
         static void Main(string[] args)
         {
-            Car toyota = new Car("Toyota", "Corolla", 1999);
-            Car honda = new Car("Honda", "Civic", 2005);
-            Car bmw = new Car("BMW", "", 2020);
+            Car toyota = new Car("Toyota", "Corolla", 1999, false);
+            Car honda = new Car("Honda", "Civic", 2005, false);
+            Car audi = new Car("Audi", "A6", 2015, true);
+            Car bmw = new Car("BMW", "", 2020, true);
         }
     }
 }
