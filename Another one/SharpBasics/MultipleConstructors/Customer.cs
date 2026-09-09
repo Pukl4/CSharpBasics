04b2f43affc625f99b2d44a09c26fbb909e703ad
Pukl4 <nikitka2601@mail.ru>
2026-09-10 01:35:53 +0300

    Creating multiple constructors

---
+++ Another one/SharpBasics/MultipleConstructors/Customer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MultipleConstructors
+{
+    internal class Customer
+    {
+        internal string FirstName { get; set; }
+        internal string LastName { get; set; }
+        internal string Phone { get; set; }
+
+        // Custom Constructor
+        public Customer(string firstName, string lastName, string phone) 
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Phone = phone;
+        }
+        public Customer(string firstName)
+        {
+            FirstName = firstName;
+        }
+    }
+}
---
+++ Another one/SharpBasics/MultipleConstructors/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace MultipleConstructors
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer ronny = new Customer("Ronny", "Round", "111-111-111");
+            Customer bonny = new Customer("Bonny");
+
+            Console.WriteLine($"{ronny.LastName}\n{bonny.FirstName}");
+        }
+    }
+}
